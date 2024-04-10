e9cb5ddc1d1d54d36adb25d4a47ce7bd7fc4a1fc
Marippp <94555323+Marippp@users.noreply.github.com>
2024-04-10 10:44:54 -0400

    Ajout de la page de connexion et pouvoir se créer un compte

--- HideOrLose/App.js
+++ HideOrLose/App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React, { useEffect, useState } from "react";
+import 'react-native-gesture-handler';
+import LoginPage from './Components/LoginPage';
+import HomePage from './Components/HomePage';
+import RegisterPage from './Components/RegisterPage';
+const Stack = createStackNavigator();
 
-export default function App() {
+import {firebase} from "./firebaseConfig";
+
+function App() {
+
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function onAuthStateChanged(user)
+  {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={LoginPage} options={{headerShown:false}}/>
+        <Stack.Screen name='RegisterPage' component={RegisterPage} options={{headerShown:false}}/>
+      </Stack.Navigator>
+    )
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name='HomePage' component={HomePage} options={{headerShown:false}}/>
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+  return (
+    <NavigationContainer>
+      <App/>
+    </NavigationContainer>
+  )
+}
---
+++ HideOrLose/Components/HomePage.js
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, TextInput, Image } from 'react-native';
+import React, {useEffect, useState} from "react"
+import { useNavigation } from '@react-navigation/native';
+
+export default function DashboardPage() {
+  const navigation = useNavigation();
+  return (
+    <View style = {styles.container}>
+
+    <Text style ={styles.titleText}>Bonjour</Text>
+
+    <TouchableOpacity
+      style = {styles.button}
+      onPress={()=>navigation.navigate("Login")}
+    >
+      <Text style={styles.buttonText}>Se déconnecter</Text>
+    </TouchableOpacity>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 100,
+},
+titleText: {
+    fontSize: 26,
+    fontWeight: "bold",
+    marginLeft: 10,
+    marginTop: 10
+},
+button: {
+    marginTop: 20,
+    height: 70,
+    width: 250,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#F4A460",
+    borderRadius: 50
+},
+buttonText: {
+    fontWeight: "bold",
+    fontSize: 22
+},
+});
