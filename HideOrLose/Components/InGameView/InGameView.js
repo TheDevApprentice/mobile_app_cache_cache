87c49097bd637ae0def942591a72eb63784594df
habric <80226527+TheDevApprentice@users.noreply.github.com>
2024-05-06 16:13:54 -0400

    End of the day
    On a fait la mise en place des écran et la communication entre le serveur

--- HideOrLose/Components/InGameView/InGameView.js
+++ HideOrLose/Components/InGameView/InGameView.js
@@ -23,16 +23,22 @@ export default function InGameView({socket}) {
     time: 999999999, 
     nbEliminated: 0
   }); // Information de la game en cours
-
+  const [isHunter, setIsHunter] = useState(false);
 
   useEffect(() => {
     _requestLocationPermission();
     _subscribe();
-      socket.on('update-game',(room)=>{setRoom(room)});
-      console.log(room)
+      socket.on('update-game',
+      (room)=>{
+        setRoom(room); 
+        socket.emit("user-game-update", {myPosition}); 
+      });
+      socket.on('is-hunter',()=>{setIsHunter(true)});
+      // console.log(room)
     return () => {
       _unsubscribe();
       socket.off('update-game');
+      socket.off('is-hunter');
     };
   }, []);
 
@@ -46,10 +52,11 @@ export default function InGameView({socket}) {
   const _subscribe = () => {
     setSubscription(
       Magnetometer.addListener((data) => {
+        _getCurrentLocation();
+
         setMagnetometer(_angle(data));
       })
     );
-    _getCurrentLocation();
   };
 
   const _unsubscribe = () => {
@@ -60,7 +67,7 @@ export default function InGameView({socket}) {
   const _getCurrentLocation = async () => {
     try {
       let { coords } = await Location.getCurrentPositionAsync({});
-      console.log("Current Position:", coords);
+      // console.log("Current Position:", coords);
       setMyPosition({ lattitude: coords.latitude, longitude: coords.longitude });
     } catch (error) {
       console.error("Error getting current location:", error);
@@ -132,7 +139,105 @@ export default function InGameView({socket}) {
   };
 
   return (
-    <Grid style={{ backgroundColor: 'black' }}>
+    isHunter ? (
+      <Grid style={{ backgroundColor: 'black' }}>
+        <Row style={{ alignItems: 'center' }} size={.9}>
+          <Col style={{ alignItems: 'center' }}>
+            <Text
+              style={{
+                color: '#fff',
+                fontSize: height / 26,
+                fontWeight: 'bold'
+              }}>
+                Chasseur
+            </Text>
+          </Col>
+        </Row>
+
+        <Row style={{ alignItems: 'center' }} size={.1}>
+          <Col style={{ alignItems: 'center' }}>
+            <View style={{ position: 'absolute', width: width, alignItems: 'center', top: 0 }}>
+              <Image source={require('../../assets/compass_pointer.png')} style={{
+                height: height / 26,
+                resizeMode: 'contain'
+              }} />
+            </View>
+          </Col>
+        </Row>
+
+        <Row style={{ alignItems: 'center' }} size={2}>
+          <Col style={{ 
+            alignItems: 'center', 
+            justifyContent: 'center', 
+            position: 'relative', 
+            transform: [
+                {
+                  rotate: `${360 - magnetometer}deg`
+                }
+              ]
+          }}>
+            <Image source={require("../../assets/compass_bg.png")} style={{
+              height: width - 80,
+              justifyContent: 'center',
+              alignItems: 'center',
+              resizeMode: 'contain',
+              // transform: [{ rotate: 360 - magnetometer + 'deg' }]
+            }} />
+
+            {/* Point indiquant la direction de l'user en game */}
+            {room.users.map((user) => {
+              // {console.log(user)}
+                        <View 
+                        style={{
+                          position: 'absolute',
+                            transform: [
+                              {
+                                rotate: `${_getUserDirection(user.coordinate) }deg`
+                              }
+                            ]
+                          }}
+                        >
+                          <View style={{
+                            width: 15,
+                            height: 15,
+                            marginTop: -136,
+                            borderRadius: 7.5,
+                            backgroundColor: 'red',
+                          }}
+                        />
+                      </View>
+            })}
+        </Col>
+        </Row>
+
+        <Row style={{ alignItems: 'center' }} size={1}>
+          <Col style={{ alignItems: 'center' }}>
+            <Text style={{ color: '#fff' }}>Nombre d'éliminés : {room.nbEliminated} / {room.users.length}</Text>
+            <TouchableOpacity 
+                    style={{    
+                      borderWidth: 1,
+                      backgroundColor: '#DC143C',
+                      borderRadius: 5,
+                      paddingLeft: 5,
+                      paddingRight: 5,
+                      height: 40,
+                      marginTop: 10, 
+                      fontSize: 70, 
+                      justifyContent: "center", 
+                      alignContent: "center"
+                    }}
+                    onPress={()=>{socket.emit('eliminate-user')}}
+                >
+                    <Text style={{color:"white"}}>Jt'ai trouvé !</Text>
+            </TouchableOpacity>
+            
+          </Col>
+        </Row>
+      </Grid>
+    )
+    : 
+    (
+      <Grid style={{ backgroundColor: 'black' }}>
       <Row style={{ alignItems: 'center' }} size={.9}>
         <Col style={{ alignItems: 'center' }}>
           <Text
@@ -141,7 +246,7 @@ export default function InGameView({socket}) {
               fontSize: height / 26,
               fontWeight: 'bold'
             }}>
-              Chasseur
+              Chassé
           </Text>
         </Col>
       </Row>
@@ -205,25 +310,10 @@ export default function InGameView({socket}) {
       <Row style={{ alignItems: 'center' }} size={1}>
         <Col style={{ alignItems: 'center' }}>
           <Text style={{ color: '#fff' }}>Nombre d'éliminés : {room.nbEliminated} / {room.users.length}</Text>
-          <TouchableOpacity 
-                  style={{    
-                    borderWidth: 1,
-                    backgroundColor: '#DC143C',
-                    borderRadius: 5,
-                    paddingLeft: 5,
-                    paddingRight: 5,
-                    height: 40,
-                    marginTop: 10, 
-                    fontSize: 70, 
-                    justifyContent: "center", 
-                    alignContent: "center"
-                  }}
-              >
-                  <Text style={{color:"white"}}>Jt'ai trouvé !</Text>
-          </TouchableOpacity>
-          
         </Col>
       </Row>
-    </Grid>
+      </Grid>
+    )
+
   );
 }
